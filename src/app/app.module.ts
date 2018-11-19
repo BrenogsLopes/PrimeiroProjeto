4b0f24ee7405d1780e3303e5c71b43b4ccff8fc0
BrenogsLopes <44807054+BrenogsLopes@users.noreply.github.com>
2018-11-19 14:16:39 -0200

    meu primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { PokemonFavoritoComponent } from './pokemon-favorito/pokemon-favorito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaLoginComponent,
+    PokemonFavoritoComponent
   ],
   imports: [
     BrowserModule,
