1f90d9a8bd2816ee879d487e3d3d89f3c595e689
BrenogsLopes <44807054+BrenogsLopes@users.noreply.github.com>
2018-11-21 12:16:18 -0200

    pagina 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { TelaLoginComponent } from './tela-login/tela-login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaErroComponent } from './pagina-erro/pagina-erro.component';
+import { PokemonFavoritoComponent } from './pokemon-favorito/pokemon-favorito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tela-login', component: TelaLoginComponent},
+  {path:'pagina-erro', component: PaginaErroComponent },
+  {path:'pokemon-favorito', component: PokemonFavoritoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
